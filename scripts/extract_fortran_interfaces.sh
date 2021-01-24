0593556eb06bb1f7ef178517811c795227630c94
M. Valin <mfvalin@gmail.com>
2021-01-24 14:30:16 -0500

    get rid of blank lines, add interface / end interface

--- scripts/extract_fortran_interfaces.sh
+++ scripts/extract_fortran_interfaces.sh
@@ -9,5 +9,7 @@ FILE_BASE=${FILE_NAME_ONLY%.*}
 
 sed -e 's/^\(.*\)$/! &/' < ${SCRIPT_DIR}/common_header.txt
 echo -e "\n! This file has been generated from ${FILE_NAME_ONLY}"
-sed -n '/F_StArT/,/F_EnD/p' ${1:-UnknownFile} | sed -e 's:^[/!]*[ ]*::' -e 's/.*F_EnD.*//' -e 's/.*F_StArT.*//'
+echo "interface"
+sed -n '/F_StArT/,/F_EnD/p' ${1:-UnknownFile} | sed -e 's:^[/!]*[ ]*::' -e 's/.*F_EnD.*//' -e 's/.*F_StArT.*//' | grep -v '^$'
+echo "end interface"
 
