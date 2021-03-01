5ebed88b18271f492ee14f3d42c476e0ad54dfd6
M. Valin <mfvalin@gmail.com>
2021-03-01 14:35:19 -0500

    user functions and serializing macros

---
+++ src/ioserver_functions.F90
@@ -0,0 +1,13 @@
+!  functions for C and FORTRAN programming
+!  Copyright (C) 2021  Recherche en Prevision Numerique
+! 
+!  This software is free software; you can redistribute it and/or
+!  modify it under the terms of the GNU Lesser General Public
+!  License as published by the Free Software Foundation,
+!  version 2.1 of the License.
+! 
+!  This software is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!  Lesser General Public License for more details.
+!
