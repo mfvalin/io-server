0a7e54c1322f73b6776e4df806302827db77b0b0
M. Valin <mfvalin@gmail.com>
2021-05-04 14:38:37 -0400

    test now in its own source file for serailizer

---
+++ tests/test_serializer.F90
@@ -0,0 +1,184 @@
+!  serializer test
+!  Copyright (C) 2021  Recherche en Prevision Numerique
+! 
+!  This software is free software; you can redistribute it and/or
+!  modify it under the terms of the GNU Lesser General Public
+!  License as published by the Free Software Foundation,
+!  version 2.1 of the License.
+! 
+!  This software is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!  Lesser General Public License for more details.
+
+#include <serializer.hf>
+program test_jar
+  implicit none
+  call test_free
+  call test_pickling
+end program
+
+subroutine test_pickling
+  use data_serialize
+  implicit none
+  logical :: old_debug
+  JAR_DECLARE(my_jar)
+  integer :: ok, ne, i
+  type :: machin1
+    integer, dimension(3) :: ip
+    integer               :: date
+    character(len=4)      :: nom
+    character(len=2)      :: typ
+    character(len=2)      :: tmp
+  end type
+  type(machin1) :: a1, x1
+
+  type :: machin2
+    integer               :: l1
+    integer               :: l2
+    character(len=2)      :: unit
+  end type
+  type(machin2), dimension(4) :: a2, x2
+
+  print *,'==========================================================='
+
+  ne = JARSPACE_ITEM(a1)
+  print *,'each machin1 scalar item will use',ne,' cells in jar'
+  ne = JARSPACE_ITEM(a2)
+  print *,'a machin2 array / array section will use',ne,' cells per array element in jar'
+
+  old_debug = my_jar%debug(.true.)
+  if(.not. JAR_VALID(my_jar)) print *,'SUCCESS: jar not valid before creation'
+  if(JAR_VALID(my_jar))       print *,'ERROR:   jar is valid before creation'
+  ok = JAR_CREATE(my_jar, 4096)
+  if(JAR_VALID(my_jar)) print *,'jar created successfully'
+  print 1,'(test_pickling) my_jar : ok, size, avail =',ok, my_jar%usable(), my_jar%avail()
+
+  a1 = machin1([0,1,2],-123456,'xxxx','yy','zz')
+  ne = JAR_PUT_SINGLE(my_jar, a1)
+  print 1,'(test_pickling) my_jar : ne, size, avail =',ne, my_jar%usable(), my_jar%avail()
+
+  do i = 1, size(a2)
+    a2(i) = machin2(500+i, 600+i, 'X_')
+  enddo
+  ne = JAR_PUT_MULTI(my_jar, a2(2:3))
+  print 1,'(test_pickling) my_jar : ne, size, avail =',ne, my_jar%usable(), my_jar%avail()
+
+  x1 = machin1([-1,-1,-1],999999,'    ','  ','  ')
+  ne = JAR_GET_SINGLE(my_jar, x1)
+  print *,a1
+  print *,x1
+  print 1,'(test_pickling) my_jar : ne, size, avail =',ne, my_jar%usable(), my_jar%avail()
+  x2 = machin2(-1, -1, '**')
+  ne = JAR_GET_MULTI(my_jar, x2(1:2))
+  print *,a2(2)
+  print *,x2(1)
+  print *,a2(3)
+  print *,x2(2)
+  print 1,'(test_pickling) my_jar : ne, size, avail =',ne, my_jar%usable(), my_jar%avail()
+  call my_jar%print(15)
+
+  JAR_REWIND(my_jar)
+
+  print *,''
+  JAR_RESET(my_jar)
+  call my_jar%print(20)
+  print 1,'(test_pickling) my_jar reset : size, avail =',my_jar%usable(), my_jar%avail()
+  ne = JAR_PUT_SINGLE_AT(my_jar, a1, 2)                        ! skip one position, start injectiong at 2 rather than 1
+  print 1,'(test_pickling) my_jar : ne, size, avail =',ne, my_jar%usable(), my_jar%avail()
+  call my_jar%print(20)
+  ne = JAR_PUT_MULTI_AT(my_jar, a2(2:4), my_jar%stored()+2)    ! skip one position, start at top + 2
+  print 1,'(test_pickling) my_jar : ne, size, avail =',ne, my_jar%usable(), my_jar%avail()
+  call my_jar%print(20)
+
+  x1 = machin1([-1,-1,-1],999999,'    ','  ','  ')
+  ne = JAR_GET_SINGLE_AT(my_jar, x1, 2)                        ! skip one position, start injectiong at 2 rather than 1
+  print *,a1
+  print *,x1
+  x2 = machin2(-1, -1, '**')
+  ne = JAR_GET_MULTI_AT(my_jar, x2(1:2), ne+2)                 ! skip one position, start at bot + 2 rather than bot +1
+  print *,a2(2)
+  print *,x2(1)
+  print *,a2(3)
+  print *,x2(2)
+  ne = JAR_GET_MULTI(my_jar, x2(3:3))
+  print *,a2(4)
+  print *,x2(3)
+
+  ok = JAR_FREE(my_jar)
+  if(.not. JAR_VALID(my_jar)) print *,'SUCCESS: jar is not valid after free'
+  if(JAR_VALID(my_jar))       print *,'ERROR:   jar is valid after free'
+
+1 format(A,10I8)
+end subroutine test_pickling
+
+subroutine test_free
+  use data_serialize
+  implicit none
+  type(jar) :: myjar
+  logical :: old_debug, scrap
+  integer :: ok
+
+  old_debug = myjar%debug(.false.)
+  print *,'old debug mode =',old_debug
+  old_debug = myjar%debug(.true.)
+  print *,'old debug mode =',old_debug
+  scrap = myjar%debug(.true.)
+  print *,'old debug mode =',scrap
+  print *,''
+
+  ok = myjar%new(640)
+  print *,'(test_free) myjar%new() =',ok, myjar%usable()
+  ok = myjar%free()
+  if(ok .eq. 0) print *,'INFO: jar datafreed'
+  ok = myjar%free()
+  if(ok .ne. 0) print *,'WARNING: jar data already freed'
+  print *,''
+
+  call test_jar_1
+  print *,''
+  old_debug = myjar%debug(.false.)
+  print *,''
+  print *,'test_free: old debug mode =',old_debug
+  old_debug = myjar%debug(.false.)
+  print *,'test_free: old debug mode =',old_debug
+  
+end subroutine test_free
+
+subroutine test_jar_1
+  use data_serialize
+  implicit none
+  integer :: ok
+  type(jar) :: myjar
+
+  ok = myjar%new(1280)
+  call test_jar_finalize
+  print *,''
+
+  block
+    type(jar) :: myjar          ! overrrides myjar in main scope
+    ok = myjar%new(128)
+    print *,'myjar%new() =',ok, myjar%usable()
+!     ok = myjar%free()
+    print *,'jar going out of scope in block'
+  end block
+  print *,''
+
+!   print *,'freeing jar at end of test_jar_1, capacity =',myjar%usable()
+!   ok = myjar%free()
+!   if(ok .ne. 0) print *,'WARNING: jar data already freed'
+  print *,'myjar%new() test_jar_1 =',ok, myjar%usable()
+  print *,'jar going out of scope in test_jar_1'
+end subroutine test_jar_1
+
+subroutine test_jar_finalize
+  use data_serialize
+  implicit none
+  type(jar) :: myjar
+  integer :: ok
+
+  ok = myjar%new(1024)
+  print *,'myjar%new(1024) =',ok, myjar%usable()
+  if(myjar%usable() .ne. 1024) print *,'unexpected size'
+  print *,'jar going out of scope in subroutine test_jar_finalize'
+end subroutine test_jar_finalize
