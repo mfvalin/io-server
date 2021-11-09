4d6725c9731a146ca98a1201a76f5cb7439b7ad2
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2021-11-09 16:33:37 +0000

    Enabling CI tests through sici000 runners

---
+++ tests/script/multi_node_tests.sh
@@ -0,0 +1,2 @@
+${MPI_COMMAND} -n ${NUM_CPUS} tests/io_server_model_write  1 2 2 1
+${MPI_COMMAND} -n ${NUM_CPUS} tests/io_server_put_error
---
+++ tests/script/single_node_tests.sh
@@ -0,0 +1,11 @@
+tests/io_server_circular_buffer_single_thread
+tests/io_server_cb_unaligned_input
+${MPI_COMMAND} -n 4           tests/io_server_circular_buffer
+${MPI_COMMAND} -n 4           tests/io_server_circular_buffer_fill
+${MPI_COMMAND} -n ${NUM_CPUS} tests/io_server_distributed_circular_buffer
+${MPI_COMMAND} -n 12          tests/io_server_simple_mutex
+${MPI_COMMAND} -n 4           tests/io_server_test_memory_arena
+${MPI_COMMAND} -n ${NUM_CPUS} tests/io_server_model_write                 1 2 2 1
+${MPI_COMMAND} -n 3           tests/io_server_dcb_edge_case_1
+${MPI_COMMAND} -n 3           tests/io_server_dcb_unaligned_input
+#${MPI_COMMAND} -n 20          tests/io_server_put_error
