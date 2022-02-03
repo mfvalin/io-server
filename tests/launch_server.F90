ace5a2e9dc08abfd2fa0ea21400f0e49b16e7885
Vincent Magnoux <vmagnoux20@gmail.com>
2022-02-03 09:04:50 -0500

    Added (untested) launcher for server only

---
+++ tests/launch_server.F90
@@ -0,0 +1,44 @@
+! Copyright (C) 2022  Environnement et Changement climatique Canada
+!
+! This is free software; you can redistribute it and/or
+! modify it under the terms of the GNU Lesser General Public
+! License as published by the Free Software Foundation,
+! version 2.1 of the License.
+!
+! This software is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! Lesser General Public License for more details.
+!
+! You should have received a copy of the GNU Lesser General Public
+! License along with this software; if not, write to the
+! Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+! Boston, MA 02111-1307, USA.
+!
+! Authors:
+!     M. Valin,   Recherche en Prevision Numerique, 2020-2022
+!     V. Magnoux, Recherche en Prevision Numerique, 2020-2022
+
+program launch_server
+  use mpi_f08
+  use ioserver_run_module
+
+  type(ioserver_input_parameters) :: params
+  logical :: success
+
+  call MPI_Init()
+
+  params % num_relay_per_node       = 2 ! Should not be important for the server
+  params % num_server_bound_server  = 2
+  params % num_grid_processors      = 1
+  params % num_channels             = 2
+
+  success = ioserver_run_server_node(params)
+
+  if (.not. success) then
+    print *, 'ERROR while running the IO server'
+    ! error stop 1 
+  end if
+
+  call MPI_Finalize()
+end program launch_server
