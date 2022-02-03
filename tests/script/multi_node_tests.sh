c283551c884bb1cc6e2adaf0c21cb603fa1a6ee5
Vincent Magnoux <vmagnoux20@gmail.com>
2022-02-03 15:08:01 +0000

    Fixed test scripts so that they successfully fail when the tests fail (although they should pass now)

--- tests/script/multi_node_tests.sh
+++ tests/script/multi_node_tests.sh
@@ -1,3 +1,3 @@
-${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_default_model
-${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_model_write  1 2 2 2
+${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_default_model               || exit -1
+${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_model_write  1 2 2 2 2      || exit -1
 # ${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_put_error  # This test currently fails on XC50
--- tests/script/single_node_tests.sh
+++ tests/script/single_node_tests.sh
@@ -1,13 +1,13 @@
-${BUILD_PATH}/tests/io_server_circular_buffer_single_thread
-${BUILD_PATH}/tests/io_server_cb_unaligned_input
-${MPI_COMMAND} -n 4           ${BUILD_PATH}/tests/io_server_circular_buffer
-${MPI_COMMAND} -n 4           ${BUILD_PATH}/tests/io_server_circular_buffer_fill
-${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_distributed_circular_buffer
-${MPI_COMMAND} -n 8           ${BUILD_PATH}/tests/io_server_shmem_heap_basic
-${MPI_COMMAND} -n 12          ${BUILD_PATH}/tests/io_server_simple_mutex
-${MPI_COMMAND} -n 4           ${BUILD_PATH}/tests/io_server_test_memory_arena
-${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_default_model
-${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_model_write                 1 2 2 4     || exit -1
-${MPI_COMMAND} -n 3           ${BUILD_PATH}/tests/io_server_dcb_edge_case_1
-${MPI_COMMAND} -n 3           ${BUILD_PATH}/tests/io_server_dcb_unaligned_input
+${BUILD_PATH}/tests/io_server_circular_buffer_single_thread                                     || exit -1
+${BUILD_PATH}/tests/io_server_cb_unaligned_input                                                || exit -1
+${MPI_COMMAND} -n 4           ${BUILD_PATH}/tests/io_server_circular_buffer                     || exit -1
+${MPI_COMMAND} -n 4           ${BUILD_PATH}/tests/io_server_circular_buffer_fill                || exit -1
+${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_distributed_circular_buffer         || exit -1
+${MPI_COMMAND} -n 8           ${BUILD_PATH}/tests/io_server_shmem_heap_basic                    || exit -1
+${MPI_COMMAND} -n 12          ${BUILD_PATH}/tests/io_server_simple_mutex                        || exit -1
+${MPI_COMMAND} -n 4           ${BUILD_PATH}/tests/io_server_test_memory_arena                   || exit -1
+${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_default_model                       || exit -1
+${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_model_write           1 2 2 2 4     || exit -1
+${MPI_COMMAND} -n 3           ${BUILD_PATH}/tests/io_server_dcb_edge_case_1                     || exit -1
+${MPI_COMMAND} -n 3           ${BUILD_PATH}/tests/io_server_dcb_unaligned_input                 || exit -1
 #${MPI_COMMAND} -n 20         ${BUILD_PATH}/tests/io_server_put_error
