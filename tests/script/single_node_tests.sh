751c64cae362f4110e8d4dcdec4711e7a3fee6dd
Vincent Magnoux <vmagnoux20@gmail.com>
2022-03-31 17:40:47 +0000

    Compile with debug info, run failing CI test with valgrind

--- tests/script/single_node_tests.sh
+++ tests/script/single_node_tests.sh
@@ -4,7 +4,7 @@ ${BUILD_PATH}/tests/io_server_test_serializer                                   
 ${MPI_COMMAND} -n 4           ${BUILD_PATH}/tests/io_server_circular_buffer                     || exit -1
 ${MPI_COMMAND} -n 4           ${BUILD_PATH}/tests/io_server_circular_buffer_fill                || exit -1
 ${MPI_COMMAND} -n 8           ${BUILD_PATH}/tests/io_server_circular_buffer_concurrent          || exit -1
-${MPI_COMMAND} -n ${NUM_CPUS} ${BUILD_PATH}/tests/io_server_distributed_circular_buffer         || exit -1
+${MPI_COMMAND} -n ${NUM_CPUS} valgrind ${BUILD_PATH}/tests/io_server_distributed_circular_buffer         || exit -1
 ${MPI_COMMAND} -n 8           ${BUILD_PATH}/tests/io_server_shmem_heap_basic                    || exit -1
 ${MPI_COMMAND} -n 12          ${BUILD_PATH}/tests/io_server_simple_mutex                        || exit -1
 ${MPI_COMMAND} -n 4           ${BUILD_PATH}/tests/io_server_test_memory_arena                   || exit -1
